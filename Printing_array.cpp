99f9207aae90aacff9024a6be7e296d318681b68
Mishree_14 <mishreekalaria14@gmail.com>
2025-07-30 15:48:23 +0530

    Update and rename Smallest_no_in_array.cpp to Printing_array.cpp

---
+++ Printing_array.cpp
@@ -0,0 +1,16 @@
+//Mishree Kalaria
+//24070123052
+//Batch: A2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5]={47,160,89,3782,715};
+    
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<"\t";
+    }
+    
+    return 0;
+}
