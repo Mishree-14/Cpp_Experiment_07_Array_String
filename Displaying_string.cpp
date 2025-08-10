0eecb0459352fed379a0496beb0a59974fe59b49
Mishree_14 <mishreekalaria14@gmail.com>
2025-08-10 16:20:19 +0530

    Create Displaying_string.cpp

---
+++ Displaying_string.cpp
@@ -0,0 +1,19 @@
+// Mishree Kalaria
+//24070123052
+//Batch: A2
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[10];
+    
+    cout<<"Enter string: ";
+    cin>>str1;
+    
+    cout<<"Given string: "<<str1;
+
+    
+    return 0;
+}
