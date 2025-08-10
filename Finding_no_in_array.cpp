a990a83a68ee9aa4172ec3b25bc0410b0b7caae7
Mishree_14 <mishreekalaria14@gmail.com>
2025-08-10 16:16:52 +0530

    Update Finding_no_in_array.cpp

--- Finding_no_in_array.cpp
+++ Finding_no_in_array.cpp
@@ -2,23 +2,26 @@
 //24070123052
 //Batch: A2
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main() {
-    int arr[5]={52,81,50,37,89};
-    int n;
     
-    cout<<"Enter index to know th number(0-4): ";
-    cin>>n;
-    
-    for(int i=0;i<5;i++){
-        if (n==i){
-            cout<<arr[i];
-        }
-        else{
-            continue;
+    int element[5], i, j, num, flag=0, count=0;
+    for(i=0;i<5;i++) {
+        cout<<"Enter element-"<<i+1<<": ";
+        cin>>element[i];
+    }
+    cout<<"Enter element to be searched: ";
+    cin>>num;
+    for(j=0;j<5;j++) {
+        if(element[j]==num) {
+            cout<<"Position of "<<num<<": "<<j+1<<endl;
+            count++;
+            flag=1;
         }
     }
+    if(flag==0) {
+        cout<<"Element not present";
+    }
     return 0;
-}
+}    
