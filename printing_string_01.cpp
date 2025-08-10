c09e14842db9a2e965a51c5a393b4c19241dd84c
Mishree_14 <mishreekalaria14@gmail.com>
2025-08-10 16:19:40 +0530

    Create printing_string_01.cpp

---
+++ printing_string_01.cpp
@@ -0,0 +1,21 @@
+// Mishree Kalaria
+//24070123052
+//Batch: A2
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[4] = "C++";
+    char str2[] = {'C','+','+','\0'};
+    char str3[4] = {'C','+','+','\0'};
+    string str4 = "C++";
+    
+    cout<<str1<<"\n";
+    cout<<str2<<"\n";
+    cout<<str3<<"\n";
+    cout<<str4<<"\n";
+    
+    return 0;
+}
