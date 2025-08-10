43f3f7d5dc33efb6dab92792ba3b947141c90bbe
Mishree_14 <mishreekalaria14@gmail.com>
2025-08-10 16:20:54 +0530

    Create concatenation_of_string.cpp

---
+++ concatenation_of_string.cpp
@@ -0,0 +1,23 @@
+// Mishree Kalaria
+//24070123052
+//Batch: A2
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[10];
+    char str2[10];
+    
+    cout<<"Enter Firstname: ";
+    cin>>str1;
+    
+    cout<<"Enter Lastname: ";
+    cin>>str2;
+    
+    cout<<str1<<" "<<str2;
+
+    
+    return 0;
+}
